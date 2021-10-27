def390f965c628398e8252401693d3386a652879
Lakeland97 <78028724+Lakeland97@users.noreply.github.com>
2021-10-28 02:47:18 +1100

    Update power.js

--- batterytimepercentagecompact@sagrland.de/power.js
+++ batterytimepercentagecompact@sagrland.de/power.js
@@ -1,50 +1,46 @@
-const Lang = imports.lang;
-var GObject = imports.gi.GObject;
+const { GObject} = imports.gi;
 const UPower = imports.gi.UPowerGlib;
 const BaseIndicator = imports.ui.status.power.Indicator;
 
 var Indicator = GObject.registerClass(
-	{
-	GTypeName: 'Indicator'
-	},
-	class Indicator extends BaseIndicator
-	{
-   _getBatteryStatus() {
+   class Indicator extends BaseIndicator {
+   // Adapted from _getStatus of the parent.
+   _getTime() {
       let seconds = 0;
 
-      const percentage = this._proxy.Percentage + '%'
-
-      // Ensure percentage label is enabled regardless of gsettings
-      this._percentageLabel.visible = true
-
-      if (this._proxy.State == UPower.DeviceState.FULLY_CHARGED) {
-         return '\u221E';
-      } else if (this._proxy.State == UPower.DeviceState.CHARGING) {
+      if (this._proxy.State === UPower.DeviceState.FULLY_CHARGED) {
+         return '';
+      } else if (this._proxy.State === UPower.DeviceState.CHARGING) {
          seconds = this._proxy.TimeToFull;
-      } else if (this._proxy.State == UPower.DeviceState.DISCHARGING) {
+      } else if (this._proxy.State === UPower.DeviceState.DISCHARGING) {
          seconds = this._proxy.TimeToEmpty;
       } else {
          // state is one of PENDING_CHARGING, PENDING_DISCHARGING
-         return _("… (%s)").format(percentage);
+         return _('Estimating…');
       }
 
       let time = Math.round(seconds / 60);
-      if (time == 0) {
+      if (time === 0) {
          // 0 is reported when UPower does not have enough data
          // to estimate battery life
-         return _("… (%s)").format(percentage);
+         return _('Estimating…');
       }
 
-      let minutes = time % 60;
-      let hours = Math.floor(time / 60);
+      // let minutes = time % 60;
+      // let hours = Math.floor(time / 60);
+
+      let emptytime = new Date(new Date().getTime() + (seconds*1000));
+
+      let hrs = emptytime.getHours();
+      let mins = emptytime.getMinutes();
+      let ampm = (hrs % 12 == hrs && "AM" || "PM");
 
       // Translators: this is <hours>:<minutes>
-      return _("%d\u2236%02d (%s)").format(hours, minutes, percentage);
+      return _('%d\u2236%02d%s').format(hrs % 12, mins, ampm);
    }
 
    _sync() {
       super._sync();
-      this._percentageLabel.clutter_text.set_markup(this._getBatteryStatus());
+      this._percentageLabel.text = this._getTime();
    }
-}
-);
+});
