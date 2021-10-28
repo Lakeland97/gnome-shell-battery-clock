c06a57e5ff8ee1b38b039037ddc48d1938688857
Lakeland97 <78028724+Lakeland97@users.noreply.github.com>
2021-10-28 15:34:01 +1100

    Update power.js

--- batterytimepercentagecompact@lakeland97/power.js
+++ batterytimepercentagecompact@lakeland97/power.js
@@ -6,43 +6,47 @@ var Indicator = GObject.registerClass(
    class Indicator extends BaseIndicator {
    // Adapted from _getStatus of the parent.
    _getTime() {
-      let seconds = 0;
+      try {
+         let seconds = 0;
 
-      // This returns fractional outputs on dual battery laptops, rounding is a quick solution
-      const percentage = Math.round(this._proxy.Percentage) + '%';
+         // This returns fractional outputs on dual battery laptops, rounding is a quick solution
+         const percentage = Math.round(this._proxy.Percentage) + '%';
 
-      // Ensure percentage label is enabled regardless of gsettings
-      this._percentageLabel.visible = true;
+         // Ensure percentage label is enabled regardless of gsettings
+         this._percentageLabel.visible = true;
 
-      if (this._proxy.State === UPower.DeviceState.FULLY_CHARGED) {
-         return '';
-      } else if (this._proxy.State === UPower.DeviceState.CHARGING) {
-         seconds = this._proxy.TimeToFull;
-      } else if (this._proxy.State === UPower.DeviceState.DISCHARGING) {
-         seconds = this._proxy.TimeToEmpty;
+         if (this._proxy.State === UPower.DeviceState.FULLY_CHARGED) {
+            return '';
+         } else if (this._proxy.State === UPower.DeviceState.CHARGING) {
+            seconds = this._proxy.TimeToFull;
+         } else if (this._proxy.State === UPower.DeviceState.DISCHARGING) {
+            seconds = this._proxy.TimeToEmpty;
+         }
+
+         let time = Math.round(seconds / 60);
+         if (time === 0) {
+            // 0 is reported when UPower does not have enough data
+            // to estimate battery life
+            return _("… (%s)").format(percentage);
+         }
+
+         let curTime = new Date();
+         let emptyTime = new Date(curTime.getTime() + (seconds*1000));
+
+         let emptyHours = emptyTime.getHours();
+         let emptyMins = emptyTime.getMinutes();
+
+         // Translators: this is <hours>:<minutes>
+         let output = _('%d\u2236%02d %s (%s)').format(emptyHours % 12, emptyMins, (emptyHours % 12 == emptyHours && "AM" || "PM"), percentage);
+
+         // Clean-up objects to prevent a memory leak, don't know if this is required or not
+         curTime = null;
+         emptyTime = null;
+
+         return output;
+      } catch (e) {
+         logError(e, 'ExtensionError');
       }
-
-      let time = Math.round(seconds / 60);
-      if (time === 0) {
-         // 0 is reported when UPower does not have enough data
-         // to estimate battery life
-         return _("… (%s)").format(percentage);
-      }
-
-      let curTime = new Date();
-      let emptyTime = new Date(curTime.getTime() + (seconds*1000));
-
-      let emptyHours = emptyTime.getHours();
-      let emptyMins = emptyTime.getMinutes();
-
-      // Translators: this is <hours>:<minutes>
-      let output = _('%d\u2236%02d %s (%s)').format(emptyHours % 12, emptyMins, (emptyHours % 12 == emptyHours && "AM" || "PM"), percentage);
-
-      // Clean-up objects to prevent a memory leak
-      curTime.destroy(); curTime = null;
-      emptyTime.destroy(); emptyTime = null;
-
-      return output;
    }
 
    _sync() {
